938949f5ba04a276b2cd2abd7a43ac5e652e6b25
AKSHAYKUMARPATIL80 <akshaykumarpatil80@gmail.com>
2025-02-27 14:43:25 +0530

    Add files via upload

--- CustomerRepository.java
+++ CustomerRepository.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.charter.entity.*;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer,Long>{
 
 	//CustomerRepository interface is used for communicating the application with database
